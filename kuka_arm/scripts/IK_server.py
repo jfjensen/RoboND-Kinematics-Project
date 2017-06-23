b3a71bf0b272cdf46d535dc02e490cdfb0ad5bdb
Jes Fink-Jensen <jes@finkjensen.be>
2017-06-23 09:42:05 -0700

    Further work on IK

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -95,28 +95,56 @@ def handle_calculate_IK(req):
             ### theta 1 ###########################################################################
             theta1 = atan2(py,px)
 
-            P_2_4_K0 = Matrix([[px - (d7*target_matrix[0,2]) - a1*cos(theta1)],
-                               [py - (d7*target_matrix[1,2]) - a1*sin(theta1)],
-                               [pz - (d7*target_matrix[2,2]) - d1]])
-            P_2_4_K0 = P_2_4_K0.subs(s)
-            P_2_4_K0_norm = np.linalg.norm(P_2_4_K0)
+            WC = Matrix([[px - (d7*target_matrix[0,2])],
+                         [py - (d7*target_matrix[1,2])],
+                         [pz - (d7*target_matrix[2,2])]])
+            print("WC: " + str(WC))
 
-            l1 = sqrt((d4*d4) + (a3*a3))
-            l1 = l1.subs(s)
-            l1_sq = l1*l1
+            T0_2 = Matrix([[a1*cos(theta1)],
+                           [a1*sin(theta1)],
+                           [d1]])
+
+            g = WC - T0_2
+
+            print("g: " + str(g))
+
+            g = g.subs(s)
+
+            print("g subs: " + str(g))
+            g_norm = np.linalg.norm(g)
+
+            l = sqrt((d4*d4) + (a3*a3))
+            l = l.subs(s)
+            
+
+            print("g_norm: " + str(g_norm) + " l: " + str(l) + " a2: " + str(a2))
         
             ### theta 3 ###########################################################################
-            theta3_beta = acos((l1_sq + (a2*a2) - (P_2_4_K0_norm*P_2_4_K0_norm))/(2*l1*a2))
-            theta3 = -(theta3_beta) + atan2(d4, a3)
+            # theta3_D = acos((l_sq + (a2*a2) - (g_norm*g_norm))/(2*l*a2))
+            theta3_D = ((l*l) + (a2*a2) - (g_norm*g_norm))/(2*l*a2)
+            theta3_D = theta3_D.subs(s)
+            theta3_phi = atan2(sqrt(1 - theta3_D**2),theta3_D)
+
+            theta3 = -theta3_phi + atan2(d4, a3)
             theta3 = theta3.subs(s)
       
             ### theta 2 ###########################################################################
-            theta2_beta = atan2(P_2_4_K0[2], sqrt((P_2_4_K0[0]*P_2_4_K0[0])+(P_2_4_K0[1]*P_2_4_K0[1])))
-           
-            theta2_alpha = acos(((P_2_4_K0_norm*P_2_4_K0_norm) + (a2*a2) - l1_sq)/(2*P_2_4_K0_norm*a2))
-            theta2 =  (pi/2) - (theta2_beta + theta2_alpha)
+            # theta2_D = acos(((g_norm*g_norm) + (a2*a2) - l_sq)/(2*g_norm*a2))
+            theta2_D = ((g_norm*g_norm) + (a2*a2) - (l*l))/(2*g_norm*a2)
+            theta2_D = theta2_D.subs(s)
+            theta2_phi = atan2(sqrt(1 - theta2_D**2),theta2_D)
+
+            D_test = ((g_norm*g_norm) + (l*l) - (a2*a2))/(2*g_norm*l)
+            D_test = D_test.subs(s)
+            phi_test = atan2(sqrt(1 - D_test**2),D_test)
+
+            print(str(theta2_phi + theta3_phi + D_test))
+
+            theta2_beta = atan2(g[2], sqrt((g[0]*g[0])+(g[1]*g[1])))
             
+            print("theta2_beta: " + str(theta2_beta) + " theta2_phi: " + str(theta2_phi))
             
+            theta2 =  -(pi/2) + (theta2_beta + theta2_phi)
             theta2 = theta2.subs(s)
             
 
