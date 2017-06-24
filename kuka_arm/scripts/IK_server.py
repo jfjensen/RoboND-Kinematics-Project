ab7288ac501cf6b63505f7dad5f4e6c576d60b91
Jes Fink-Jensen <jes@finkjensen.be>
2017-06-24 04:44:00 -0700

    IK for theta 1-2-3 almost OK

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -84,6 +84,8 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
 
+
+
             print("p: " + str(px) + " " + str(py) +  " " + str(pz))
      
             # Calculate joint angles using Geometric IK method
@@ -91,61 +93,73 @@ def handle_calculate_IK(req):
                 tf.transformations.translation_matrix((px,py,pz)),
                 tf.transformations.quaternion_matrix((req.poses[x].orientation.x, req.poses[x].orientation.y, req.poses[x].orientation.z, req.poses[x].orientation.w)))
             # print(target_matrix)
+            # print(target_matrix[0,2])
+            # print(target_matrix[1,2])
+            # print(target_matrix[2,2])
+            R0_6 = Matrix(tf.transformations.quaternion_matrix((req.poses[x].orientation.x, 
+                                                        req.poses[x].orientation.y, 
+                                                        req.poses[x].orientation.z, 
+                                                        req.poses[x].orientation.w)))
+            print(R0_6)
+            print(R_corr)
+            R0_6_corr = R0_6 * R_corr.T
 
             ### theta 1 ###########################################################################
             theta1 = atan2(py,px)
 
-            WC = Matrix([[px - (d7*target_matrix[0,2])],
-                         [py - (d7*target_matrix[1,2])],
-                         [pz - (d7*target_matrix[2,2])]])
+            # WC = Matrix([[px - (s[d7]*target_matrix[0,2])],
+            #              [py - (s[d7]*target_matrix[1,2])],
+            #              [pz - (s[d7]*target_matrix[2,2])]])
+
+
+            WC = Matrix([[px - (s[d7]*R0_6_corr[0,2])],
+                         [py - (s[d7]*R0_6_corr[1,2])],
+                         [pz - (s[d7]*R0_6_corr[2,2])]])
+
             print("WC: " + str(WC))
 
-            T0_2 = Matrix([[a1*cos(theta1)],
-                           [a1*sin(theta1)],
-                           [d1]])
+            T0_2 = Matrix([[s[a1]*cos(theta1)],
+                           [s[a1]*sin(theta1)],
+                           [s[d1]]])
 
             g = WC - T0_2
 
             print("g: " + str(g))
 
-            g = g.subs(s)
+            g_norm = sqrt(g[0]**2 + g[1]**2 + g[2]**2)
 
-            print("g subs: " + str(g))
-            g_norm = np.linalg.norm(g)
+            l = sqrt((s[d4]**2) + (s[a3]**2))
+                        
 
-            l = sqrt((d4*d4) + (a3*a3))
-            l = l.subs(s)
-            
-
-            print("g_norm: " + str(g_norm) + " l: " + str(l) + " a2: " + str(a2))
+            print("g_norm: " + str(g_norm) + " l: " + str(l) + " a2: " + str(s[a2]))
         
             ### theta 3 ###########################################################################
-            # theta3_D = acos((l_sq + (a2*a2) - (g_norm*g_norm))/(2*l*a2))
-            theta3_D = ((l*l) + (a2*a2) - (g_norm*g_norm))/(2*l*a2)
-            theta3_D = theta3_D.subs(s)
+            
+            theta3_D = ((l**2) + (s[a2]**2) - (g_norm**2))/(2*l*s[a2])
+                        
             theta3_phi = atan2(sqrt(1 - theta3_D**2),theta3_D)
 
-            theta3 = -theta3_phi + atan2(d4, a3)
-            theta3 = theta3.subs(s)
+            theta3 = -theta3_phi + atan2(s[d4], s[a3])
+            
       
             ### theta 2 ###########################################################################
-            # theta2_D = acos(((g_norm*g_norm) + (a2*a2) - l_sq)/(2*g_norm*a2))
-            theta2_D = ((g_norm*g_norm) + (a2*a2) - (l*l))/(2*g_norm*a2)
-            theta2_D = theta2_D.subs(s)
+            
+            theta2_D = ((g_norm**2) + (s[a2]**2) - (l**2))/(2*g_norm*s[a2])
+            
             theta2_phi = atan2(sqrt(1 - theta2_D**2),theta2_D)
 
-            D_test = ((g_norm*g_norm) + (l*l) - (a2*a2))/(2*g_norm*l)
-            D_test = D_test.subs(s)
-            phi_test = atan2(sqrt(1 - D_test**2),D_test)
-
-            print(str(theta2_phi + theta3_phi + D_test))
-
-            theta2_beta = atan2(g[2], sqrt((g[0]*g[0])+(g[1]*g[1])))
+            # D_test = ((g_norm**2) + (l**2) - (s[a2]**2))/(2*g_norm*l)
             
-            print("theta2_beta: " + str(theta2_beta) + " theta2_phi: " + str(theta2_phi))
+            # phi_test = atan2(sqrt(1 - D_test**2),D_test)
+
+            # print(str(theta2_phi + theta3_phi + phi_test))
+
+            theta2_beta = atan2(g[2], sqrt((g[0]**2)+(g[1]**2)))
             
-            theta2 =  -(pi/2) + (theta2_beta + theta2_phi)
-            theta2 = theta2.subs(s)
+            # print("theta2_beta: " + str(theta2_beta) + " theta2_phi: " + str(theta2_phi))
+            
+            theta2 =  (pi/2) - theta2_phi - theta2_beta
+            # theta2 = theta2.subs(s)
             
 
             print("theta1: " + str(theta1) + " theta2: " + str(theta2) + " theta3: " + str(theta3))
